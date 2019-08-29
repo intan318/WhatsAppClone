5d833ed52ce6f141f8e763088d732117abdc8e47
Intan Yoshana Oewen <34388370+intan318@users.noreply.github.com>
2019-08-29 21:09:14 +0700

    first commit

---
+++ lib/whatsapp_home.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:whatsappclone/pages/call_screen.dart';
+import 'package:whatsappclone/pages/camera_screen.dart';
+import 'package:whatsappclone/pages/chat_screen.dart';
+import 'package:whatsappclone/pages/status_screen.dart';
+
+class WhatsAppHome extends StatefulWidget {
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome> 
+  with SingleTickerProviderStateMixin {
+
+    TabController _tabController;
+
+    @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = new TabController(vsync: this, initialIndex: 1, length: 4
+    // length = 4 karena bakal ada 4 tab
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("WhatsApp"),
+        elevation: 0.7,
+        bottom: new TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.camera_alt)),
+            new Tab(text: "CHATS"),
+            new Tab(text: "STATUS",
+            ),
+            new Tab(
+              text: "CALLS",
+            )
+            
+          ],
+        ),
+      ),
+    body: new TabBarView(
+      controller: _tabController,
+      children: <Widget>[
+        new CameraScreen(),
+        new ChatScreen(),
+        new StatusScreen(),
+        new CallsScreen(),
+      ],
+    ),
+    floatingActionButton: new FloatingActionButton(
+      backgroundColor: Theme.of(context).accentColor,
+      child: new Icon(Icons.message),
+      onPressed: ()=>print("open chats"),
+    ),  
+    );
+  }
+}
